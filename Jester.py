80503fd7b18f0bd405029f57499d5a5ce0c2b64b
pvvuuren <32298792+pvvuuren@users.noreply.github.com>
2017-10-26 08:48:02 +0200

    Changed channel back to Cloak & Dagger

--- Jester.py
+++ Jester.py
@@ -12,11 +12,11 @@ async def background_loop():
     messages = deepcopy(messages_list)
     cleanmessages = deepcopy(messages_list)
     while not client.is_closed:
-        channel = client.get_channel("360701697963851776")
+        channel = client.get_channel("242956769503084544")
         if not messages:
             messages = deepcopy(cleanmessages)
         await client.send_message(channel, messages.pop(random.randrange(0, len(messages))))
-        await asyncio.sleep(90)
+        await asyncio.sleep(21600)
 
 client.loop.create_task(background_loop())
 client.run("MzYwNjg1NTM0NTk2ODI1MDkx.DKuSoQ.Prdbtg0W66L_WztPOqO-nDOgzWE")
