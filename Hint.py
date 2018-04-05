64a739b1870242492b338793ad83002b39b39f38
pvvuuren <32298792+pvvuuren@users.noreply.github.com>
2018-04-05 09:03:46 +0200

    Changed code

--- Hint.py
+++ Hint.py
@@ -12,14 +12,17 @@ client = discord.Client()
 async def background_loop():
     await client.wait_until_ready()
     await client.change_presence(game=discord.Game(name="Cloak & Dagger"))
+    last_message = None
     messages = deepcopy(messages_list)
     cleanmessages = deepcopy(messages_list)
     while not client.is_closed:
         channel = client.get_channel("242956769503084544")
         if not messages:
             messages = deepcopy(cleanmessages)
-        await client.send_message(channel, messages.pop(random.randrange(0, len(messages))))
-        await asyncio.sleep(21600)
+        if len([x async for x in client.logs_from(channel, limit=2, after=last_message)]) > 1:
+            content = messages.pop(random.randrange(0, len(messages)))
+            last_message = await client.send_message(channel, content)
+            await asyncio.sleep(21600)
 
 client.loop.create_task(background_loop())
 client.run(key)
