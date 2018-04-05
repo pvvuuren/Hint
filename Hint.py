35ced2f17e9e2e825feb0bd896ca82b4bde2ddcf
pvvuuren <32298792+pvvuuren@users.noreply.github.com>
2018-04-05 09:40:11 +0200

    Changed post time back to 6 hours

--- Hint.py
+++ Hint.py
@@ -22,7 +22,7 @@ async def background_loop():
         if len([x async for x in client.logs_from(channel, limit=2, after=last_message)]) > 1:
             content = messages.pop(random.randrange(0, len(messages)))
             last_message = await client.send_message(channel, content)
-            await asyncio.sleep(60)
+            await asyncio.sleep(21600)
 
 client.loop.create_task(background_loop())
 client.run(key)
